bebd7f95a5b4e30b226f78ce19309e814378d687
vnling <veenling@gmail.com>
2020-12-05 16:09:15 +0400

    MVP final version, fixed most bugs except layout

--- server.js
+++ server.js
@@ -16,29 +16,37 @@ const listener = app.listen(port, () => {
     console.log("Your app is listening on port " + listener.address().port);
 });
 
+//model gets information from .env file
 const model = new HostedModel({
     url: process.env.RUNWAYURL,
     token: process.env.RUNWAYTOKEN
 });
 
+//read the scifi file so we can sample text from it
 fs.readFile(filename, "utf-8", function(err, data){
     if(err) throw err;
     lines = data.replace(/\n$/, '').split('\n');
  })
 
+ //get a random line from the file, assign to actualText
 function getRandomLine() {
     let firstLine = lines[Math.floor(Math.random()*lines.length)];
     let actualText = firstLine;
     let counter = 0;
     let i = Math.floor(Math.random()*lines.length + 1);
+    //while the text is shorter than 300 chars, keep adding to the text
     while (actualText.length < 300) {
         let tempStr = lines[i + counter];
         counter++;
         actualText = actualText.concat(tempStr);
     }
+    //return the firstline of the sample as prompt for ML models
+    //return the full text as human sample
     return [firstLine, actualText];
 }
 
+//sends request to runway model
+//taken from runway-ml-template glitch app
 app.post("/runwayml", async (request, response) => {
     console.log(request.body);
 
@@ -57,6 +65,7 @@ app.post("/runwayml", async (request, response) => {
 
 });
 
+//returns the random line of scifi when requested by script.js
 app.get("/scifi-random", async (request, response) => {
     let reply = {
         message: getRandomLine(),
